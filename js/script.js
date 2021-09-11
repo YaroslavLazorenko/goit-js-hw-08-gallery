ee5a42b35e33610bb436c77953bd1a92ef4ccd95
YaroslavLazorenko <lazorenko85@gmail.com>
2021-09-11 17:19:36 +0300

    Complete task in script.js

--- js/script.js
+++ js/script.js
@@ -33,25 +33,75 @@ function onGalleryNavClick(evt) {
   const isImageElem = evt.target.nodeName === 'IMG';
 
   if (!isImageElem) return;
-    const bigImageURL = evt.target.dataset.source;
-    const imageAltAttrValue = evt.target.getAttribute("alt");
+  const bigImageURL = evt.target.dataset.source;
+  const imageAltAttrValue = evt.target.getAttribute('alt');
 
   openModal(bigImageURL, imageAltAttrValue);
 }
 
 function openModal(imageURL, imageAltAttrValue) {
-    const modalBoxElem = document.querySelector('.js-lightbox');
-    const modalImageElem = document.querySelector(".lightbox__image");
-    const closeModalBtn = document.querySelector('[data-action="close-lightbox"]');
+  const modalBoxElem = document.querySelector('.js-lightbox');
+  const modalImageElem = document.querySelector('.lightbox__image');
+  const closeModalBtnElem = document.querySelector('[data-action="close-lightbox"]');
+  const modalBackdropElem = document.querySelector('.lightbox__overlay');
 
-    modalBoxElem.classList.add('is-open');
-    modalImageElem.setAttribute("src", imageURL);
-    modalImageElem.setAttribute("alt", imageAltAttrValue);
+  modalBoxElem.classList.add('is-open');
+  modalImageElem.setAttribute('src', imageURL);
+  modalImageElem.setAttribute('alt', imageAltAttrValue);
 
-    closeModalBtn.addEventListener("click", onCloseModalBtnClick);
+  closeModalBtnElem.addEventListener('click', onCloseModalBtnClick);
+  modalBackdropElem.addEventListener('click', onModalBackdropClick);
+  window.addEventListener('keydown', onKeyboardBtnClick);
 
-    function onCloseModalBtnClick() {
-        modalBoxElem.classList.remove('is-open');
-        modalImageElem.setAttribute("src", "");
+  function closeModal() {
+    modalBoxElem.classList.remove('is-open');
+    modalImageElem.setAttribute('src', '');
+
+    closeModalBtnElem.removeEventListener('click', onCloseModalBtnClick);
+    modalBackdropElem.removeEventListener('click', onModalBackdropClick);
+    window.removeEventListener('keydown', onKeyboardBtnClick);
+  }
+
+  function onCloseModalBtnClick() {
+    closeModal();
+  }
+
+  function onModalBackdropClick() {
+    closeModal();
+  }
+
+  function onKeyboardBtnClick({ key }) {
+    const isEscBtnPressed = key === 'Escape';
+    const isLeftBtnPressed = key === 'ArrowLeft';
+    const isRightBtnPressed = key === 'ArrowRight';
+
+    if (isEscBtnPressed) closeModal();
+    if (isLeftBtnPressed) {
+      const currentGalleryItemIndex = galleryItems.indexOf(
+        galleryItems.find(item => item.original === imageURL),
+        );
+        
+      if (currentGalleryItemIndex > 0) {
+          const newGalleryItemIndex = currentGalleryItemIndex - 1;
+          
+        modalImageElem.setAttribute('src', galleryItems[newGalleryItemIndex].original);
+        modalImageElem.setAttribute('alt', galleryItems[newGalleryItemIndex].description);
+        imageURL = galleryItems[newGalleryItemIndex].original;
+      }
     }
+
+    if (isRightBtnPressed) {
+      const currentGalleryItemIndex = galleryItems.indexOf(
+        galleryItems.find(item => item.original === imageURL),
+        );
+        
+      if (currentGalleryItemIndex < galleryItems.length - 1) {
+          const newGalleryItemIndex = currentGalleryItemIndex + 1;
+          
+        modalImageElem.setAttribute('src', galleryItems[newGalleryItemIndex].original);
+        modalImageElem.setAttribute('alt', galleryItems[newGalleryItemIndex].description);
+        imageURL = galleryItems[newGalleryItemIndex].original;
+      }
+    }
+  }
 }
